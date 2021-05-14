dfb74af892e294a3cbca2cb2ea25debc6f2a2d45
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-14 00:56:36 -0500

    Reto cumplido de list comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,9 +1,18 @@
 def run():
+    '''
     squares = []
     for i in range(1,101):
         if i % 3 != 0:
             squares.append(i**2)
+    
+    squares = [i**2 for i in range(1,101) if i%3 != 0]
     print(squares)
+    '''
+    #Reto
+    #Crear, con un list comprehension, una lista de todos los múltiplos de 4 que la vez también son múltiplos de 6 y de 9, hasta 5 digitos.
+
+    multiples = [i for i in range (1,100000) if i % 36 == 0 ]
+    print(multiples)
 
 if __name__ == '__main__':
     run()
