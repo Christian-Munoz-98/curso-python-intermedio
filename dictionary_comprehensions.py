5af80f7e1ef6527119aec660c93e172c976ef588
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-14 01:32:32 -0500

    Reto: diccionario 1000 numeros y raiz cuadrada

--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -1,3 +1,5 @@
+import math
+
 def run():
     # my_dict = {}
     
@@ -5,7 +7,12 @@ def run():
     #     if i % 3 != 0:
     #         my_dict[i] = i**3
 
-    my_dict = {i: i**3 for i in range (1,101) if i % 3 !=0}
+    # my_dict = {i: i**3 for i in range (1,101) if i % 3 !=0}
+
+    #Reto
+    #Crear, con un dictionary comprehension, un diccionatio cuyas llaves sean los 1000 primeros números naturales con sus raíces cuadradas como valores
+
+    my_dict = {i: math.sqrt(i) for i in range (1,1001)}
     
     print(my_dict)
 
