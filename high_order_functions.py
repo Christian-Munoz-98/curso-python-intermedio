73200badee303fee6af4f0146a9eb117de5d9167
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-17 11:42:21 -0500

    Uso de la función filter

--- high_order_functions.py
+++ high_order_functions.py
@@ -2,3 +2,8 @@
 my_list = [1, 4, 5, 6, 9, 13, 19, 21]
 odd_comprehension = [i for i in my_list if i%2 != 0]
 print(odd_comprehension)
+
+#Uso con filter
+my_list = [1,4,5,6,9,13,19,21]
+odd = list(filter(lambda x: x%2 != my_list))
+print(odd)
