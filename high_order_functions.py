f91f76766dc29f9f9351678c2cbee8841eeeb250
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-17 12:13:24 -0500

    Cuadrado por list comprehension

--- high_order_functions.py
+++ high_order_functions.py
@@ -1,3 +1,5 @@
+#Función "Filter"
+'''
 #Filtrando una lista con list comprehensions
 my_list = [1, 4, 5, 6, 9, 13, 19, 21]
 odd_comprehension = [i for i in my_list if i%2 != 0]
@@ -7,3 +9,10 @@ print(odd_comprehension)
 my_list = [1,4,5,6,9,13,19,21]
 odd = list(filter(lambda x: x%2 != 0, my_list))
 print(odd)
+'''
+#Función "Map"
+
+#Elevando una lista al cuadrado con list comprehension
+my_list = [1,2,3,4,5]
+odd_comprehension = [i**2 for i in my_list ]
+print(odd_comprehension)
