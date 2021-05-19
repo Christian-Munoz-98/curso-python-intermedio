9fcff9d455280bfc2af280b451598ad5ce666364
Christian Munoz <chgeovany.15@gmail.com>
2021-05-19 02:10:22 -0500

    Antes del proyecto del ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,16 @@
+#Al ejecutar el script la pantalla va a mostrar el mensaje "Adivina la palabra" junto con guiones bajos que representan la palabra a adivinar
+#Debemos ir ingresando letras hasta adivinar la palabra
+
+#Incorpora comprehensions, manejo de errores y manejo de archivos
+#Utiliza el archivo data.txt y leelo para obtener palabras
+#Investiga la funcion "enumerate"
+#El metodo get de los diccionarios puede servirte.
+'''
+La sentencia
+os.system("cls") ->Windows
+os.system("clear") -> Unix
+Te servira para limpiar pantalla
+'''
+#anade un sistema de puntos
+#Dibuja al "ahorcado" en cada jugada con codigo ascii
+#mejora la interfaz
