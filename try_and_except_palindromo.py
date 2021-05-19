e956c396b7b4b36a7e22f378f4dc2c505e9386ec
Christian Munoz <chgeovany.15@gmail.com>
2021-05-18 23:54:48 -0500

    Prueba de errores

--- try_and_except_palindromo.py
+++ try_and_except_palindromo.py
@@ -3,12 +3,19 @@ def palindromo(string):
 
 def run():
 
-    string = input("Ingresa una palabra:")
+    # string = input("Ingresa una palabra:")
 
-    if palindromo(string):
-        print("Es palindromo")
-    else:
-        print("No es palindromo")
+    # if palindromo(string):
+    #     print("Es palindromo")
+    # else:
+    #     print("No es palindromo")
+    
+    try:
+        print(palindromo(1))
+    except TypeError:
+        print("Solo se pueden ingresar cadenas")
+
+
 
 if __name__ == '__main__':
     run()
