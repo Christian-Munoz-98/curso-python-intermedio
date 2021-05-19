b2eac3d61c8a08e8f53fa48aa7795af8bb0838f2
Christian Munoz <chgeovany.15@gmail.com>
2021-05-19 00:24:51 -0500

    Excepciones agregadas a debugging.py falta mejor la excepcion en palindromo

--- debugging.py
+++ debugging.py
@@ -3,9 +3,17 @@ def divisors(number):
     return divisors
 
 def run():
-    number = int(input("Dame una cantidad---->>>>"))
-    print(f"los divisores de {number} son {divisors(number)}")  
-    print("Termino mi programa")
+    try:
+        number = int(input("Dame una cantidad---->>>>"))
+        if number < 0:
+            raise Exception ('No se permiten numeros negativos')
+        print(f"los divisores de {number} son {divisors(number)}")  
+        print("Termino mi programa")
+    except ValueError:
+        print("Debes ingresar un numero")
+    except Exception:
+        print("No se permiten numeros negativos")
+
 
 if __name__ == '__main__':
     run()
--- try_and_except_palindromo.py
+++ try_and_except_palindromo.py
@@ -2,17 +2,17 @@ def palindromo(string):
     return string == string[::-1]
 
 def run():
-
-    # string = input("Ingresa una palabra:")
-
-    # if palindromo(string):
-    #     print("Es palindromo")
-    # else:
-    #     print("No es palindromo")
-    
     try:
-        print(palindromo(1))
-    except TypeError:
+        string = input("Ingresa una palabra:")
+
+        if palindromo(string):
+            print("Es palindromo")
+        else:
+            print("No es palindromo")
+        if type(int(string)) == int:
+            raise ValueError("No se permiten numeros")
+
+    except ValueError:
         print("Solo se pueden ingresar cadenas")
 
 
