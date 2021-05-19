058e6899c00a2031331759d6a5b1167fba83ac95
Christian Munoz <chgeovany.15@gmail.com>
2021-05-19 00:57:14 -0500

    Assert statement para numero negativo

--- assert_statemens.py
+++ assert_statemens.py
@@ -2,11 +2,18 @@ def divisors(number):
     divisors = [i for i in range (1,number+1) if number % i == 0 ]
     return divisors
 
+# def run():
+    
+#     number = (input("Dame una cantidad---->>>>"))
+#     assert number.isnumeric(), "Debes ingresar un numero"
+#     print(f"los divisores de {int(number)} son {divisors(int(number))}")  
+#     print("Termino mi programa")
+
 def run():
     
-    number = (input("Dame una cantidad---->>>>"))
-    assert number.isnumeric(), "Debes ingresar un numero"
-    print(f"los divisores de {int(number)} son {divisors(int(number))}")  
+    number = int(input("Dame una cantidad---->>>>"))
+    assert number > 0, "Debes ingresar un numero positivo"
+    print(f"los divisores de {number} son {divisors(number)}")  
     print("Termino mi programa")
 
 
