005251ed58a6bd2c28acda7bdc47fecc3a102b4d
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-20 12:44:29 -0500

    Mi versión del juego del ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,16 +1,36 @@
-#Al ejecutar el script la pantalla va a mostrar el mensaje "Adivina la palabra" junto con guiones bajos que representan la palabra a adivinar
-#Debemos ir ingresando letras hasta adivinar la palabra
-
-#Incorpora comprehensions, manejo de errores y manejo de archivos
-#Utiliza el archivo data.txt y leelo para obtener palabras
+#Incorpora manejo de errores
 #Investiga la funcion "enumerate"
-#El metodo get de los diccionarios puede servirte.
-'''
-La sentencia
-os.system("cls") ->Windows
-os.system("clear") -> Unix
-Te servira para limpiar pantalla
-'''
 #anade un sistema de puntos
 #Dibuja al "ahorcado" en cada jugada con codigo ascii
 #mejora la interfaz
+
+import random 
+import os
+
+def lista_palanbras():
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        palabras = [line.strip() for line in f]
+    return palabras
+
+def run():
+    print("Bienvenido al juego del ahorcado")
+    palabra = random.choice(lista_palanbras())
+    palabra_secreta = "_" * len(palabra)
+    palabra_secreta_control = list("_" * len(palabra))
+    
+    while palabra != palabra_secreta:
+
+        os.system ("cls") 
+        print("Bienvenido al juego del ahorcado")
+        print(palabra_secreta)
+
+        caracter = input(("\nEscribe una letra: "))
+
+        for i in range(len(palabra)):
+            if caracter == palabra[i]:
+                palabra_secreta_control[i] = caracter
+                palabra_secreta = "".join(palabra_secreta_control)
+        print(f"¡Felicidades, Gnaste!. La palabra es {palabra}")
+
+if __name__ == '__main__':
+    run()
