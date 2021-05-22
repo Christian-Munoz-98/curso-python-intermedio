bb8b9fb16b2f73d1381b33e427db63c7d96deacb
Christian Muñoz <chgeovany.15@gmail.com>
2021-05-21 23:07:02 -0500

    Nuevos cambios y archivos

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,4 @@
-import random 
-import os
+import random, os
 
 def lista_palabras():
     with open("./archivos/data.txt", "r", encoding="utf-8") as f:
@@ -7,23 +6,90 @@ def lista_palabras():
     return palabras
 
 def run():
+
+    AHORCADO = ['''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========''']
+
     palabra = random.choice(lista_palabras())
     palabra_secreta = "_" * len(palabra)
-    palabra_secreta_control = list("_" * len(palabra))
+    palabra_secreta_control = list(palabra_secreta)
+    errores = 0
     
-    while palabra != palabra_secreta:
-
-        os.system ("cls") 
-        print("Bienvenido al juego del ahorcado")
+    while errores < 10:
+        # os.system("cls")
+        print("AHORCADO")
+        print(AHORCADO[errores])
         print(palabra_secreta)
 
         caracter = input(("\nEscribe una letra: "))
-
-        for i in range(len(palabra)):
-            if caracter == palabra[i]:
-                palabra_secreta_control[i] = caracter
-                palabra_secreta = "".join(palabra_secreta_control)
-        print(f"¡Felicidades, Gnaste!. La palabra es {palabra}")
+        if errores == 6 :
+            print("Perdiste")
+            break
+        elif palabra == palabra_secreta:
+            print(f"¡Felicidades, Gnaste!. La palabra es {palabra}")
+            break
+        elif len(caracter) != 1:
+            print("Introduce una sola letra")
+        elif caracter in palabra_secreta_control:
+            print("Ya has elegido esa letra")
+        elif caracter not in 'abcdefghijklmnopqrstuvwxyz':
+            print ('Solo puedes ingresar letras')
+        elif caracter not in palabra:
+            errores += 1
+        else:
+            for i in range(len(palabra)):
+                if caracter in palabra[i]:
+                    palabra_secreta_control[i] = caracter
+                    palabra_secreta = "".join(palabra_secreta_control)
 
 if __name__ == '__main__':
     run()
+
